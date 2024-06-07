56ab28df912f86326221724f8ccbb17c83256ebc
Uallessonivo <uallessons@gmail.com>
2024-06-07 09:09:24 -0300

    feat: Lambda expressions

---
+++ AdvancedTopics/Lambda/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace AdvancedTopics.Lambda
+{
+    public class Book
+    {
+        public string Title { get; set; } = default!;
+        public int Price { get; set; }
+    }
+}
---
+++ AdvancedTopics/Lambda/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AdvancedTopics.Lambda
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> square = number => number * number;
+            Console.WriteLine(square(5));
+            Console.WriteLine(Square(5));
+
+
+            const int factor = 5;
+            Func<int, int> multiplier = number => number * factor;
+            Console.WriteLine(multiplier(10));
+
+
+            var books = new BookRepository().GetBooks();
+            var cheaBooks = books.FindAll(book => book.Price < 10);
+
+            foreach (var book in cheaBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+
+        static int Square(int number) => number * number;
+
+        static bool IsCheaperThan10Dollars(Book book) => book.Price < 10;
+    }
+}
