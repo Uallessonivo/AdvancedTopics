65bf3e207e5bfa7f6db90edad05924389af4cdcc
Uallessonivo <uallessons@gmail.com>
2024-06-10 09:35:00 -0300

    feat: Events

---
+++ AdvancedTopics/Events/VideoEncoder.cs
@@ -0,0 +1,30 @@
+﻿namespace AdvancedTopics.Events
+{
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+
+    public class VideoEncoder
+    {
+        // EventHandler
+        // EventHandler<TEventArgs>
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if (VideoEncoded != null)
+            {
+                VideoEncoded(this, new VideoEventArgs() { Video = video });
+            }
+        }
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(video);
+        }
+    }
+}
