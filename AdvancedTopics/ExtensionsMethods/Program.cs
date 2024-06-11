1fe7219e0aea44c4d9b1d88feb4fca827404fc67
Uallessonivo <uallessons@gmail.com>
2024-06-11 08:31:29 -0300

    feat: Extensions Methods

---
+++ AdvancedTopics/ExtensionsMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AdvancedTopics.ExtensionsMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long blog post...";
+            var shortenedPost = post.Shorten(5);
+
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 4, 10, 24, 6 };
+            var max = numbers.Max();
+
+            Console.WriteLine(shortenedPost);
+            Console.WriteLine(max);
+        }
+    }
+}
