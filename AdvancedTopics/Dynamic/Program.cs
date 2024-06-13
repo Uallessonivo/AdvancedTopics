572879f71f95fdea124f12ee530dac75a0f75496
Uallessonivo <uallessons@gmail.com>
2024-06-13 08:11:10 -0300

    feat: Dynamics

---
+++ AdvancedTopics/Dynamic/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AdvancedTopics.Dynamic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = "Mosh";
+            obj.GetHashCode();
+
+            var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            methodInfo.Invoke(null, null);
+
+            dynamic excelObject = "mosh";
+            excelObject.Optimize();
+        }
+    }
+}
