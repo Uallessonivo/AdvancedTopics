3c71e4ee9cc27ca201c0d44f587dbf6a1df12f9d
Uallessonivo <uallessons@gmail.com>
2024-06-13 08:11:18 -0300

    feat: NullableTypes

---
+++ AdvancedTopics/NullableTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AdvancedTopics.NullableTypes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime? date = null;
+            DateTime date2 = date ?? DateTime.Now;
+            DateTime date3 = (date != null) ? date.GetValueOrDefault() : DateTime.Today;
+
+            Console.WriteLine("GetValueOrDefault(): " + date.GetValueOrDefault());
+            Console.WriteLine("HasValue: " + date.HasValue);
+            Console.WriteLine("Value: " + date.Value);
+            Console.WriteLine(date2);
+            Console.WriteLine(date3);
+        }
+    }
+}
